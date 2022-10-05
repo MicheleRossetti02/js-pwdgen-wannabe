e7368f078f60426fc8c2d050e73eaa08bc8043c4
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-05 16:55:23 +0200

    complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,31 @@ const name = prompt ('What is your name');
 const surname = prompt ('What is your surname');
 const color = prompt('What is your favoureit color?');
 const number = 22;
-let string ='DO NOT SHARE IT WITH ANYONE';
+// const share ='DO NOT SHARE IT WITH ANYONE';
+
+const password=name + surname + color + number;
 
 console.log(name)
 console.log(surname)
 console.log(color)
+// console.log(share)
+console.log(password)
 
-console.log('Ciao ' + name + ' ' + surname + ' la tua nuova password è')
+console.log ('Ciao ' + name + ' ' + surname + ' la tua nuova password è')
 
-console.log(
+ console.log (
     `
-    
 ${name}${surname}${color}${number}
-${string}
 
     `
 
 )
+
+document.getElementById('info').innerHTML ='Ciao ' + name + ' ' + surname + ' la tua nuova password è';
+
+
+// document.write(password);
+document.getElementById('password').innerHTML= name + surname + color + number;
+
+
+document.getElementById('share').innerHTML =  'DO NOT SHARE IT WITH ANYONE ';
